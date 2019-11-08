f6e70a49c92e067bbebcd4ea6e046749f9d92ddd
rwalker <robpub@robonco.com>
2019-11-08 06:12:35 -0600

    Compare two objects

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
+// no require should in this File.  
+// It gets added to Object.prototype in the other spec for entire test execution.
 
 describe('Basic Mocah Test', function() {
   it('should deal with objects', function() {
     var obj = {name: 'Robert', gender: 'male'};
+    var objB = {name: 'Robert', gender: 'male'};
     obj.should.have.property('name').equal('Robert');
+    obj.should.deep.equal(objB);
   });
 });
