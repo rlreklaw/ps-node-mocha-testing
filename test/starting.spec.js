4d43398ac15a6df03ad0f3ab547e64b05c1ca253
rwalker <robpub@robonco.com>
2019-11-08 06:08:36 -0600

    Test an object

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
 
 describe('Basic Mocah Test', function() {
-  it('should throw errors', function() {
-    assert.equal(3, 3);
-  })
+  it('should deal with objects', function() {
+    var obj = {name: 'Robert', gender: 'male'};
+    obj.should.have.property('name').equal('Robert');
+  });
 });
